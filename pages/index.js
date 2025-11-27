fcad8836687b0fa9a1fb6318684f720f7356cf99
Allan Medeiros <15980763+allanmedeiros71@users.noreply.github.com>
2025-11-27 22:18:27 +0000

    Atualiza a estrutura da página inicial para incluir um título e um vídeo do YouTube

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
     return (
-        <h1>Pra tu minha princesa</h1>
+        <html>
+            <h1>Tó para tu! Torpedo.</h1>
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/TM4G9qzHPCk?si=o-t4WKbC5jDdpMB6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </html>
     )
 }
 
