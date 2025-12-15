80fa141d65c535c9cf017f22de0026809c3c5b6f
Allan Medeiros <medeiros@codata.pb.gov.br>
2025-12-15 08:11:21 -0300

    feat: add SSL configuration for production environment in database connection pool

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,10 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 /**
